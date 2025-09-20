bca0d0e262372fc83c7f400332f1be28603b4972
Thomas Baylot <thomasbaylot@MacBook-Air-de-Thomas.local>
2025-09-20 23:59:02 +0200

    Add first prototype

---
+++ Prototype_clean.py
@@ -0,0 +1,7 @@
+# Dashboard with equity (SPY), forex (USD), commmodities (gold (GC=F), crude oil (WTI or USO), wheat(ZW=F)), bonds (^TNX)(inflation linked bonds)
+# Data about growth, inflation, volatility and yield
+# Data goes bask as far as possible w/ widget slider to adjust time frame
+
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
